40fcdbf42d47f3415f4121dd261e0799f5c7a1a4
rubenbot2 <76092760+rubenbot2@users.noreply.github.com>
2021-10-08 04:13:13 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,6 +13,7 @@ apt-get install ffmpeg libwebp
 apt-get install nodejs
 apt-get install tesseract
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
+npm install terminal-kit
 npm install
 npm audit fix
 
